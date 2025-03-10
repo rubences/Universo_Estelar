d927e20947a4251dd7aee6f73bf09904e1adb8dc
rubences <91731294+rubences@users.noreply.github.com>
2025-03-10 11:36:36 +0000

    cosas

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def main():
     print(estrella1)  # Output: (1, 2, 3)
     print(estrella2)  # Output: (4, 5, 6)
     print(estrella1.galaxia())  # Output: Galaxia Desconocida
+    print(estrella2.galaxia())  # Output: Galaxia Desconocida
     print(estrella1.distancia(estrella2))  # Output: 5.196152422706632
+    print(estrella2.distancia(estrella1))  # Output: 5.196152422706632
 
 if __name__ == "__main__":
     main()
